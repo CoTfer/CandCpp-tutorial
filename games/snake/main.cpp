dee33433f365a7e5c0de80145a159e26b2e208a7
CoTfer <1033479364@qq.com>
2022-07-22 20:40:04 +0800

    初始化
    1.使用EasyX进行构建，构建前打开games/EasyX/EasyX_20220610.exe进行环境安装
    2.定义了蛇的结构体以及相关函数

--- games/snake/main.cpp
+++ games/snake/main.cpp
@@ -1,6 +1,49 @@
-#include <stdio.h>
+#include <graphics.h>
+#include <conio.h>
+
+#define GRAPH_WIDTH 1280		// 画布宽度
+#define GRAPH_HEIGHT 800		// 画布高度
+#define UNIT_PIXEL 10			// 单元像素
+
+// 定义蛇的结构体（类）
+struct Snake {
+	int x, y, right, bottom, length, width, toward;
+};
+
+// 初始化蛇参数
+void initsnake(Snake* s, int x, int y, int length, int width, int toward = 1) {
+	s->x = x;
+	s->y = y;
+	s->right = x + length * UNIT_PIXEL;
+	s->bottom = y + width * UNIT_PIXEL;
+	s->length = length;
+	s->width = width;
+}
+
+void movesnake(Snake* s, int frame = 10) {
+	int index = 0;
+	while (true) {
+		if (index == frame) {
+			s->x += UNIT_PIXEL;
+			index = 0;
+		}
+		else
+			index++;
+	}
+}
+
+void get_key_code() {
+	char in = '0';
+	while (in = _getch())
+		outtext(in);
+}
 
 int main() {
-    
-    return 0;
+	initgraph(GRAPH_WIDTH, GRAPH_HEIGHT);		// 创建绘图窗口
+	Snake* s = (Snake *)malloc(sizeof(Snake));		// 实例化蛇
+	initsnake(s, 200, 200, 3, 1, 1);				// 初始化蛇的参数
+	fillrectangle(s->x, s->y, s->right, s->bottom);
+	//movesnake(s);
+	get_key_code();
+	return 0;
 }
